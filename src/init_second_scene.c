f561caddb8d4e3e0bd60ee9a1ef14aacc825307e
CORGE cédric <cedric.corge@localhost.localdomain>
2021-04-07 10:10:03 +0200

    game scene

--- src/init_second_scene.c
+++ src/init_second_scene.c
@@ -9,7 +9,11 @@
 
 int init_objs_2(scene_t *scene)
 {
-    scene[1].objs[0] = NULL;
+    if (init_object(scene[1].objs[0], "assets/start.png", \
+(sfVector2f){0, 0}, (sfIntRect){0, 0, 722, 638}) == -1)
+        return (-1);
+    sfSprite_setScale(scene[1].objs[0]->sprite, (sfVector2f){2.66, 1.7});
+    scene[1].objs[1] = NULL;
     return (0);
 }
 
@@ -21,7 +25,7 @@ int init_buttons_2(scene_t *scene)
 
 int init_second_scene(scene_t *scene)
 {
-    if (malloc_scene(scene, 1, 1, 1) == -1)
+    if (malloc_scene(scene, 1, 2, 1) == -1)
         return (-1);
     scene[1].music = NULL;
     if (init_buttons_2(scene) == -1 || init_objs_2(scene) == -1)
