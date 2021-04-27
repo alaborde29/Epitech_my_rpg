2dfb786a4fcf004b26af6dcf60d2ef87710d008f
Henri Chauvet <henri.chauvet@epitech.eu>
2021-04-28 01:20:47 +0200

    Fixing valgrind error

--- lib/my/my_strdup.c
+++ lib/my/my_strdup.c
@@ -12,7 +12,7 @@ int my_strlen(char const *str);
 char *my_strdup(char const *src)
 {
     int i = 0;
-    char *src2 = malloc(sizeof(char) * my_strlen(src));
+    char *src2 = malloc(sizeof(char) * my_strlen(src) + 1);
 
     while (src[i] != '\0'){
         src2[i] = src[i];
